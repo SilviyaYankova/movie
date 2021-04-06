0c0cd66720553cab5874c3b67f5c031a35fa7b97
SilviyaYankova <silviya_yankova91@abv.bg>
2021-04-07 01:25:41 +0300

    Added user roles manageable from the app.

--- src/main/java/bg/softuni/movie/exceptions/ObjectNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package bg.softuni.movie.exceptions;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.NOT_FOUND)
-public class ObjectNotFoundException extends RuntimeException{
-
-}
--- src/main/java/bg/softuni/movie/model/entity/enums/UserRoleEnum.java
+++
@@ -1,6 +0,0 @@
-package bg.softuni.movie.model.entity.enums;
-
-public enum UserRoleEnum {
-    ADMIN,
-    USER
-}
